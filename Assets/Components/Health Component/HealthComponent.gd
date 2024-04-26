72d92963cfaaa028da1f436f32637af9e8a47912
Artemouli <jfrogg.au@gmail.com>
2024-04-26 17:08:42 +0930

    Initial Enemy ai

--- Assets/Components/Health Component/HealthComponent.gd
+++ Assets/Components/Health Component/HealthComponent.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 var max_health: int
 var current_health: int
+
+
--- Assets/Prefabs/Enemy/TestEnemy.gd
+++ Assets/Prefabs/Enemy/TestEnemy.gd
@@ -2,9 +2,20 @@ extends CharacterBody2D
 
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
 
-func _ready():
-	pass 
+const speed = 550
+
+#@export var player: Node2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
 func _physics_process(_delta: float) -> void:
 	#print(player.global_position)
-	pass
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+	
+	
+func make_path() -> void:
+	nav_agent.target_position = player.global_position
+
+func _on_timer_timeout():
+	make_path()
